39df2c528ac550eb264aa88130f8176941c45e34
Nedfire23 <15346748+Nedfire2347@users.noreply.github.com>
2020-01-11 18:19:55 +0100

    Update mod_monitor.sh

--- wifi/mod_monitor.sh
+++ wifi/mod_monitor.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
-airmon-ng check-kill
-ip link set wlan0 down
-iw dev wlan0 set type monitor
+if [ $# -eq 2 ]; then
+	var=$2
+else
+	var="wlan0"
+fi
+
+if [ "$1" = 'start' ]; then
+	echo "Enabling $var Monitor mode" 
+	airmon-ng check kill
+	ip link set $var down
+	iw dev $var set type monitor
+	service NetworkManager start
+
+elif [ "$1" = 'stop' ]; then
+	echo "Disabling $var Monitor mode"
+	airmon-ng check kill
+	ip link set $var down
+	iw dev $var set type managed
+	service NetworkManager restart
+
+else
+	echo '\n\tUsage :\n \tstart : Starting Monitor Mode \n\tstop : Disabling Monitor Mode'
+	echo '\tYou can precise your Interface : '
+	echo "\t\tex : start wlan0"
+fi
